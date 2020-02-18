3805d9115bb311258feb3944c2b0cb63bc2449fd
unknown <joehene@gmail.com>
2020-02-18 04:51:25 +0200

    Adding flts document template view

--- scripts/flts_cert_template_view.sql
+++ scripts/flts_cert_template_view.sql
@@ -1,12 +1,13 @@
-CREATE VIEW cb_plot_vw_lht_certificate_template AS
+CREATE OR REPLACE VIEW cb_plot_vw_lht_certificate_template AS
 SELECT cb_plot.id,
-    cb_scheme.scheme_name,
+    cb_scheme.scheme_name AS scheme_name,
     cb_check_lht_land_rights_office.value AS land_rights_office,
     concat(cb_holder.holder_first_name, ' ', cb_holder.holder_surname) AS full_name,
-    cb_holder.holder_identifier,
+    cb_holder.holder_identifier AS holder_id,
     cb_check_lht_marital_status.value AS marital_status,
     cb_plot.plot_number,
     cb_plot.upi,
+    cb_plot.geom AS plot_geom,
     st_collect(cb_plot.geom) OVER (PARTITION BY cb_plot.scheme_id) AS scheme_plots,
     cb_check_lht_relevant_authority.value AS type_of_relevant_authority,
     cb_relevant_authority.name_of_relevant_authority,
