f0c3fb0f9384a83e01c53b9a5881ad396fb9d228
unknown <joehene@gmail.com>
2020-02-19 07:13:18 +0200

    commented nature of marriage

--- scripts/flts_cert_template_view.sql
+++ scripts/flts_cert_template_view.sql
@@ -1,10 +1,12 @@
 CREATE OR REPLACE VIEW cb_plot_vw_lht_certificate_template AS
 SELECT cb_plot.id,
     cb_scheme.scheme_name AS scheme_name,
+    cb_scheme.scheme_number AS scheme_number,
     cb_check_lht_land_rights_office.value AS land_rights_office,
     concat(cb_holder.holder_first_name, ' ', cb_holder.holder_surname) AS full_name,
     cb_holder.holder_identifier AS holder_id,
     cb_check_lht_marital_status.value AS marital_status,
+--     cb_check_lht_nature_of_marriage.value AS nature_of_marriage,
     cb_plot.plot_number,
     cb_plot.upi,
     cb_plot.geom AS plot_geom,
@@ -20,6 +22,7 @@ SELECT cb_plot.id,
      JOIN cb_check_lht_land_rights_office ON cb_scheme.land_rights_office = cb_check_lht_land_rights_office.id
      JOIN cb_holder ON cb_social_tenure_relationship.holder_id = cb_holder.id
      JOIN cb_check_lht_marital_status ON cb_check_lht_marital_status.id = cb_holder.marital_status
+--      JOIN cb_check_lht_nature_of_marriage ON cb_check_lht_nature_of_marriage.id = cb_holder.nature_of_marriage
      JOIN cb_check_lht_relevant_authority ON cb_check_lht_relevant_authority.id = cb_scheme.relevant_authority
      JOIN cb_relevant_authority ON cb_relevant_authority.id = cb_scheme.relevant_authority
      JOIN cb_check_lht_reg_division ON cb_check_lht_reg_division.id = cb_scheme.registration_division
