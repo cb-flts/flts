207d6cb79c0adae336aae0ef9230a31ee1318c05
unknown <joehene@gmail.com>
2020-02-21 07:45:01 +0200

    enabled user name on comments

--- ui/flts/workflow_manager/config.py
+++ ui/flts/workflow_manager/config.py
@@ -1307,10 +1307,10 @@ configurations = {
         'comment_save': [
             SaveColumn(
                 column='comment', value=None, entity='Comment'
+            ),
+            SaveColumn(
+                column='user_id', value=8, entity='Comment'
             )
-            # SaveColumn(
-            #     column='user_id', value=1, entity='Comment'
-            # )
             # SaveColumn(
             #     column='timestamp', value=datetime.now(), entity='Comment'
             # )
