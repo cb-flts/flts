04031b9ccc7b6048f0ca8e89e7c82442b299eafb
unknown <joehene@gmail.com>
2020-03-26 09:54:08 +0300

    Refactored plot styling in certificate preprocess

--- ui/flts/doc_generate_utils.py
+++ ui/flts/doc_generate_utils.py
@@ -19,11 +19,14 @@ email                : joehene@gmail.com
 import time
 
 from PyQt4.QtCore import (
+    Qt,
     QDateTime,
     QFile
 )
 from qgis.core import (
-    QgsMapLayerRegistry
+    QgsMapLayerRegistry,
+    QgsRuleBasedRendererV2,
+    QgsSymbolV2
 )
 from qgis.utils import iface
 from qgis.core import QgsApplication
@@ -43,13 +46,59 @@ STYLE_FILE = '{0}/scheme_plots_label.qml'.format(STYLES_DIR)
 
 def certificate_preprocess(plot, plots):
     """
-    Utility function that loads plots that belng to a specific scheme.
+    Utility function that loads and renders plots that belong to a specific
+    scheme.
     """
     scheme_plot_layer = lht_plot_layer(plot.scheme_id, CERTIFICATE_PLOT)
     QgsMapLayerRegistry.instance().addMapLayer(scheme_plot_layer)
-    if QFile.exists(STYLE_FILE):
-        scheme_plot_layer.loadNamedStyle(STYLE_FILE)
-        scheme_plot_layer.triggerRepaint()
+    # if QFile.exists(STYLE_FILE):
+    #     scheme_plot_layer.loadNamedStyle(STYLE_FILE)
+    #     scheme_plot_layer.triggerRepaint()
+    # iface.mapCanvas().setExtent(scheme_plot_layer.extent())
+    # QgsApplication.processEvents()
+
+    # Styling reference plot using primary key
+    filter_exp = '"id" = ' + str(plot.id)
+    scheme_symbol = QgsSymbolV2.defaultSymbol(
+        scheme_plot_layer.geometryType()
+    )
+    rule_renderer = QgsRuleBasedRendererV2(scheme_symbol)
+    root_rule = rule_renderer.rootRule()
+
+    # Rule for highlighting reference plot
+    scheme_rule = root_rule.children()[0].clone()
+    scheme_rule.setLabel('Reference Plot')
+    scheme_rule.setFilterExpression(filter_exp)
+    scheme_symbol_layer = scheme_rule.symbol().symbolLayer(0)
+    scheme_symbol_layer.setFillColor(Qt.yellow)
+    scheme_symbol_layer.setOutlineColor(Qt.black)
+    root_rule.appendChild(scheme_rule)
+
+    # Rule for other plots
+    def_rule = root_rule.children()[0].clone()
+    def_rule.setLabel('Plots')
+    def_rule.setIsElse(True)
+    def_symbol_layer = def_rule.symbol().symbolLayer(0)
+    def_symbol_layer.setFillColor(Qt.transparent)
+    def_symbol_layer.setOutlineColor(Qt.black)
+    root_rule.appendChild(def_rule)
+
+    # Remove default rule
+    root_rule.removeChildAt(0)
+
+    # Set renderer
+    scheme_plot_layer.setRendererV2(rule_renderer)
+
+    # Enable labeling
+    scheme_plot_layer.setCustomProperty("labeling", "pal")
+    scheme_plot_layer.setCustomProperty("labeling/enabled", "true")
+    scheme_plot_layer.setCustomProperty("labeling/fontFamily", "Arial")
+    scheme_plot_layer.setCustomProperty("labeling/fontSize", "8")
+    scheme_plot_layer.setCustomProperty("labeling/fieldName", "plot_number")
+    scheme_plot_layer.setCustomProperty("labeling/placement", "0")
+
+    scheme_plot_layer.triggerRepaint()
+
     iface.mapCanvas().setExtent(scheme_plot_layer.extent())
     QgsApplication.processEvents()
 
