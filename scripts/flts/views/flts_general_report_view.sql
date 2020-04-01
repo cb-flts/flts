5733e020d1c83473589ce4e4b788a8de5a7d20a4
unknown <joehene@gmail.com>
2020-04-01 05:54:38 +0300

    Updated report views

--- scripts/flts/views/flts_general_report_view.sql
+++
@@ -1,2 +0,0 @@
-CREATE OR REPLACE VIEW cb_holder_vw_lht_report_template AS
-    SELECT COUNT(*) FROM cb_social_tenure_relationship
--- scripts/flts/views/flts_report_age_grp_template_view.sql
+++
@@ -1,2 +0,0 @@
-CREATE OR REPLACE VIEW cb_holder_vw_gender_report_template AS
-    SELECT COUNT(*) FROM cb_social_tenure_relationship
