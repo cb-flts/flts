6b8956235eb0ab553610297e9056512cbcfe6605
John G <gkahiu@gmail.com>
2020-04-13 22:08:37 +0300

    Use QGIS type factories for column value representation

--- ui/flts/search/search_model.py
+++ ui/flts/search/search_model.py
@@ -17,11 +17,15 @@ email                : stdm@unhabitat.org
  *                                                                         *
  ***************************************************************************/
 """
+from datetime import datetime
+
 from PyQt4.QtCore import (
     QAbstractTableModel,
     QModelIndex,
     Qt
 )
+
+from qgis.gui import QgsEditorWidgetRegistry
 
 
 class SearchResultsModel(QAbstractTableModel):
@@ -29,13 +33,23 @@ class SearchResultsModel(QAbstractTableModel):
     def __init__(self, ds_manager, parent=None):
         super(SearchResultsModel, self).__init__(parent)
         self._ds_manager = ds_manager
+        self._vector_layer = self._ds_manager.vector_layer
         self._config = self._ds_manager.search_config
         self._res_features = []
         self._field_idx_name = {}
         # Store index of 'id' column
         self._id_col_idx = -1
-
         self._update_field_index_name()
+
+        # Container for widget factories based on field type. Key: column
+        # name, value: widget factory
+        self._col_widget_factories = {}
+        self._col_widget_config = {}
+        self._col_cache = {}
+        self._layer_col_idx = {}
+
+        # Populate the column mappings
+        self._populate_widget_factories()
 
     @property
     def datasource_manager(self):
@@ -49,6 +63,48 @@ class SearchResultsModel(QAbstractTableModel):
         # Update the mapping of field names based on the index.
         col_names = self._ds_manager.valid_column_mapping.keys()
         self._field_idx_name = {i:col_names[i] for i in range(len(col_names))}
+
+    def _populate_widget_factories(self):
+        """
+        We use the widget factories for proper representation of string
+        values for different field types. This is preferred to writing custom
+        value formatters based on the data type.
+        """
+        if not self._vector_layer.isValid():
+            return
+
+        fields = self._vector_layer.dataProvider().fields()
+        for i in range(fields.count()):
+            field_name = fields.at(i).name()
+            widget_type = self._vector_layer.editFormConfig().widgetType(i)
+            widget_factory = QgsEditorWidgetRegistry.instance().factory(
+                widget_type
+            )
+            if widget_factory:
+                self._col_widget_factories[field_name] = widget_factory
+                widget_config = self._vector_layer.editFormConfig().\
+                    widgetConfig(i)
+                self._col_widget_config[field_name] = widget_config
+                self._layer_col_idx[field_name] = i
+                cache = widget_factory.createCache(
+                    self._vector_layer,
+                    i,
+                    widget_config
+                )
+                self._col_cache[field_name] = cache
+
+    def _column_string_representation(self, col_name, value):
+        # Returns a friendly string representation of the column value.
+        if not col_name in self._col_widget_factories:
+            return value
+
+        return self._col_widget_factories.get(col_name).representValue(
+            self._vector_layer,
+            self._layer_col_idx[col_name],
+            self._col_widget_config[col_name],
+            self._col_cache[col_name],
+            value
+        )
 
     def columnCount(self, index=QModelIndex()):
         # Returns number of columns based on the valid mapped columns in the
@@ -101,7 +157,7 @@ class SearchResultsModel(QAbstractTableModel):
                 if not col_name:
                     return None
                 val = feat.attribute(col_name)
-                return val
+                return self._column_string_representation(col_name, val)
 
         return None
 
