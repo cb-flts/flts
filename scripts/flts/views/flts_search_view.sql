e67fa0ba5dfe9e6505a6ee60108b934c778e1027
unknown <joehene@gmail.com>
2020-04-18 11:07:20 +0300

    Refactored juristic search view

--- scripts/flts/views/flts_search_view.sql
+++ scripts/flts/views/flts_search_view.sql
@@ -7,16 +7,16 @@ CREATE OR REPLACE VIEW cb_vw_plot_search AS
     cb_check_lht_plot_use.value AS plot_use,
     cb_plot.area,
     cb_scheme.scheme_name
-   FROM ((cb_plot
-     JOIN cb_check_lht_plot_use ON ((cb_check_lht_plot_use.id = cb_plot.use)))
-     JOIN cb_scheme ON ((cb_scheme.id = cb_plot.scheme_id)))
+   FROM cb_plot
+     JOIN cb_check_lht_plot_use ON cb_check_lht_plot_use.id = cb_plot.use
+     JOIN cb_scheme ON cb_scheme.id = cb_plot.scheme_id;
 
 CREATE OR REPLACE VIEW cb_vw_holder_search AS
     SELECT cb_holder.id,
         cb_holder.holder_first_name,
         cb_holder.holder_surname,
         cb_holder.holder_identifier
-       FROM cb_holder
+       FROM cb_holder;
 
 CREATE OR REPLACE VIEW cb_vw_scheme_search AS
     SELECT cb_scheme.id,
@@ -24,11 +24,11 @@ CREATE OR REPLACE VIEW cb_vw_scheme_search AS
         cb_check_lht_region.value AS region,
         cb_scheme.date_of_establishment,
         cb_scheme.date_of_approval
-       FROM (cb_scheme
-         JOIN cb_check_lht_region ON ((cb_check_lht_region.id = cb_scheme.region)))
+       FROM cb_scheme
+         JOIN cb_check_lht_region ON cb_check_lht_region.id = cb_scheme.region;
 
-CREATE OR REPLACE VIEW cb_vw_scheme_search AS
+CREATE OR REPLACE VIEW cb_vw_juristic_search AS
      SELECT cb_holder.id,
         cb_holder.juristic_person_name,
         cb_holder.juristic_person_number
-       FROM cb_holder
+       FROM cb_holder;
